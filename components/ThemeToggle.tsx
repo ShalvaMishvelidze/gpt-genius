b837c4bbda89531da09f85658bbcbe6c66815686
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-08 16:06:36 +0400

    set up toggle theme functionality and made theme toggle component more suitable for the ui

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,4 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
 const ThemeToggle = () => {
-  return <button className="btn btn-primary btn-sm">ThemeToggle</button>;
+  const [theme, setTheme] = useState(themes.dracula);
+
+  const toggleTheme = () => {
+    const newTheme = theme === themes.winter ? themes.dracula : themes.winter;
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme} className="btn btn-circle btn-sm">
+      {theme === "winter" ? (
+        <BsMoonFill className="size-4" />
+      ) : (
+        <BsSunFill className="size-4" />
+      )}
+    </button>
+  );
 };
 export default ThemeToggle;
