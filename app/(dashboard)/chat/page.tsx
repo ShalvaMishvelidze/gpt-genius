5058882230ea79705900ce0f470f59f962869f6d
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-09 12:02:26 +0400

    added react query provider to the app

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,4 +1,16 @@
-const Chat = () => {
-  return <div>Chat</div>;
+import Chat from "@/components/Chat";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const ChatPage = () => {
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Chat />
+    </HydrationBoundary>
+  );
 };
-export default Chat;
+export default ChatPage;
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode, useState } from "react";
 import { Toaster } from "react-hot-toast";
 
 const providers = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60, // 1 minutes
+          },
+        },
+      })
+  );
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster position="top-center" />
       {children}
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
+
 export default providers;
