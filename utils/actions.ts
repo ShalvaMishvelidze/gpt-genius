b39189cea72e634e980d85f807360f9c4a64657b
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-13 11:52:25 +0400

    set up a working chat

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,29 @@
 "use server";
 
-export const generateChatResponse = async (chatMessage: string) => {
-  console.log(chatMessage);
-  return "awesome";
+import { ChatMessage } from "@/interfaces";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const generateChatResponse = async (chatMessages: ChatMessage[]) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        ...chatMessages,
+      ],
+      model: "gpt-4.1-nano",
+      temperature: 0,
+      max_completion_tokens: 150,
+    });
+
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 };
