a6ba25b7a51f44e39872f259320180f1a85de909
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-14 15:28:25 +0400

    added images for tours powered by unsplash api

--- app/(dashboard)/tours/[id]/page.tsx
+++ app/(dashboard)/tours/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { TourInfo } from "@/components/TourInfo";
 import { getSingleTour } from "@/utils/actions";
+import axios from "axios";
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=`;
 
 const SingleTourPage = async ({ params }: { params: { id: string } }) => {
   const tour = await getSingleTour(params.id);
@@ -9,11 +12,27 @@ const SingleTourPage = async ({ params }: { params: { id: string } }) => {
   if (!tour) {
     return redirect("/tours");
   }
+
+  const { data } = await axios.get(`${url}${tour.city}`);
+  const image = data?.results[0]?.urls?.raw;
+
   return (
     <div>
       <Link href={"/tours"} className="btn btn-secondary mb-12">
         back to tours
       </Link>
+      {image ? (
+        <div>
+          <Image
+            src={image}
+            alt={tour.title}
+            width={300}
+            height={300}
+            className="rounded-xl shadow-xl mb-16 h-96 w-96 object-cover"
+            priority
+          />
+        </div>
+      ) : null}
       <TourInfo tour={tour} />
     </div>
   );
